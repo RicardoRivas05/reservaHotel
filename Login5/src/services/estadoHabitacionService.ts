a0468d954b96d334785c6cbafd73168d2365b331
JeslinArdon <jeslinardon@gmail.com>
2025-04-09 04:15:29 -0600

    vistas estado habitacion funcional

---
+++ Login5/src/services/estadoHabitacionService.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+
+const API_URL = 'http://localhost:3001/api/estadoHabitacion'; //Personalizar segun sea el caso
+
+export const getEstadoHabitacion = () => axios.get(API_URL);
+export const createEstadoHabitacion = (data: any) => axios.post(API_URL, data);
+export const updateEstadoHabitacion = (id: number, data: any) => axios.put(`${API_URL}/${id}`, data);
+export const deleteEstadoHabitacion = (id: number) => axios.delete(`${API_URL}/${id}`);
